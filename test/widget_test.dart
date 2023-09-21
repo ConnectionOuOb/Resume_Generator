aba11af012e334ffda2010f47e484582fd544af7
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2023-09-21 12:33:12 +0800

    2023-09-21_09:21
    Renew

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:connectionBlog/main.dart';
+import 'package:AutoResume/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
